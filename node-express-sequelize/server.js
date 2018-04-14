5e0ecfbd8557422fc9fa7ccfb9f953bc1ff83882
Nader Nasr <nadernasr7@gmail.com>
2018-04-14 09:18:30 +0300

    GET / POST

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -2,12 +2,14 @@ var express = require('express');
 var app = express();
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize('test', 'test', null, { dialect: 'sqlite', storage: './db.test.sqlite' });
+//-------------------------------------------
+var syncUser = require('./index')
 
 var User = sequelize.define('User', {
   username: Sequelize.STRING
 });
 
-
+//req,res
 var x = Sequelize.User()
 app.get('/users', function(x, err){
 	if(){
@@ -20,11 +22,13 @@ app.get('/users', function(x, err){
 
 /*  Create a '/users' route that responds to 
     a GET request with all users in the database */
-
-app.post('users', function(){
+//req,res
+app.post('/users', function(){
 	if( err ){
+		console.log('users POST has returned an ERR')
 		request.send( err )
 	} else {
+		console.log('users POST has been SUCCESSFUL')
 		request.send( data )
 	}
 
